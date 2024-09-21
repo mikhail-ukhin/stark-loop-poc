c9869b506b0aea7eee1802136269358a50a4b030
Merchezatter <mikhail.ukhin@gmail.com>
2024-09-21 09:55:45 +0700

    add abi generator

---
+++ contracts/scripts/deploy/generate_abi.ts
@@ -0,0 +1,27 @@
+import { getCompiledCode } from "./utils";
+import * as fs from 'fs';
+
+
+async function main() {
+
+  let sierraCode, casmCode;
+
+  try {
+    ({ sierraCode, casmCode } = await getCompiledCode(
+      "contracts_Starkloop"
+    ));
+  } catch (error: any) {
+    console.log("Failed to read contract files");
+    process.exit(1);
+  }
+
+  const jsonString = JSON.stringify(sierraCode.abi, null, 2);
+
+  fs.writeFileSync('artifacts/abi.json', jsonString);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
